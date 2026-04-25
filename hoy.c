fb7b26b50afbe6e32cfd9eda8c5734d49a2f8a91
alCoPaUL <paul.amurao@gmail.com>
2026-04-25 19:28:32 -0400

    Add files via upload

---
+++ hoy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+char *h = "Hellllo";
+char arr[7];
+int i;
+for (i = 0 ; i < 7 ; i++) {
+arr[6-i] = h[i];
+}
+printf("%s %s",h, arr);
+return 0;
+}
+
+// cl /I.\ hoy.c
+// hoy.c - string reverse by alCoPaUL, HAsAsIN [NaCl], beth GviLLErMo [GIMO], Zelotes ni Isa [ZnI], Brigada Ocho [b8]
+// April 25, 2026, NYC
+//
+//
+// License: Royalties Of 30 US Dollars Per Instance On A Computer And Groups Of Computers. alcopaul@yahoo.com For Transactions.
+//
+// "If You No Pay = You Male Gay."
+//
